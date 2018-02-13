0304a199527778bfb9739e50490982be10ff5374
Vitalii Yevtushenko <eltiren@eltiren.com>
2018-02-13 21:14:11 +0200

    init

---
+++ cloak/cloak.h
@@ -0,0 +1,19 @@
+//
+//  cloak.h
+//  cloak
+//
+//  Created by Vitalii Yevtushenko on 2/13/18.
+//  Copyright © 2018 Roll'n'Code. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for cloak.
+FOUNDATION_EXPORT double cloakVersionNumber;
+
+//! Project version string for cloak.
+FOUNDATION_EXPORT const unsigned char cloakVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cloak/PublicHeader.h>
+
+
